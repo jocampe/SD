1d5745793d7de14d8e51ffc19fbece09f98f1c8a
Joao <joaompedro4@gmail.com>
2020-03-30 17:33:51 +0100

    update silo client

--- silo-client/src/main/java/pt/tecnico/sauron/silo/client/SiloClientApp.java
+++ silo-client/src/main/java/pt/tecnico/sauron/silo/client/SiloClientApp.java
@@ -1,5 +1,8 @@
 package pt.tecnico.sauron.silo.client;
 
+import pt.tecnico.sauron.silo.client.SiloFrontend;
+import pt.tecnico.sauron.silo.grpc.Silo.PingRequest;
+import pt.tecnico.sauron.silo.grpc.Silo.PingResponse;
 
 public class SiloClientApp {
 	
@@ -11,6 +14,15 @@ public class SiloClientApp {
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
 		}
-	}
+		
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
 	
+		SiloFrontend frontend = new SiloFrontend(host, port);
+
+	    PingRequest request = PingRequest.newBuilder().setText("friend").build();
+	    PingResponse response = frontend.setPing(request);
+	    System.out.println(response);
+			
+	}
 }
